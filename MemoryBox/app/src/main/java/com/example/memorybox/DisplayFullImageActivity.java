6b856a0dc54df9af552a99abc7f2b6fee44cb2b0
nhokthicksock <18127147@student.hcmus.edu.vn>
2021-04-20 13:50:08 +0700

    + Navigation Bar | + Handle Share Image to Other Apps

--- MemoryBox/app/src/main/java/com/example/memorybox/DisplayFullImageActivity.java
+++ MemoryBox/app/src/main/java/com/example/memorybox/DisplayFullImageActivity.java
@@ -1,20 +1,96 @@
 package com.example.memorybox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class DisplayFullImageActivity extends AppCompatActivity {
     ImageView image;
+    Toolbar toolbar_photo;
+    BottomNavigationView bottomNavigationView_photo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_full_image);
-        image=findViewById(R.id.imageOther);
-        String pathImage=getIntent().getStringExtra("path_image");
+        image = findViewById(R.id.imageOther);
+        toolbar_photo = findViewById(R.id.toolbar_photo);
+        bottomNavigationView_photo = findViewById(R.id.navigation_bar_photo);
+        setSupportActionBar(toolbar_photo);
+
+        String pathImage = getIntent().getStringExtra("path_image");
         Glide.with(this).load(pathImage).asBitmap().into(image);
+
+        bottomNavigationView_photo.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.photo_navigation_share:
+                        BitmapDrawable bitmapDrawable = (BitmapDrawable) image.getDrawable();
+                        Bitmap bitmap = bitmapDrawable.getBitmap();
+                        shareImage(bitmap);
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+
+            private void shareImage(Bitmap bitmap) {
+                Uri uri = getImageToShare(bitmap);
+                Intent intent = new Intent(Intent.ACTION_SEND);
+
+                intent.putExtra(Intent.EXTRA_STREAM, uri);
+
+                intent.putExtra(Intent.EXTRA_TEXT, "Sharing Image");
+
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here");
+
+                intent.setType("image/png");
+
+                startActivity(Intent.createChooser(intent, "Share Via"));
+            }
+
+            private Uri getImageToShare(Bitmap bitmap) {
+                File imagefolder = new File(getCacheDir(), "images");
+                Uri uri = null;
+                try {
+                    imagefolder.mkdirs();
+                    File file = new File(imagefolder, "shared_image.png");
+                    FileOutputStream outputStream = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+                    outputStream.flush();
+                    outputStream.close();
+                    uri = FileProvider.getUriForFile(DisplayFullImageActivity.this, "com.example.memorybox", file);
+                } catch (Exception e) {
+//                    Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+                return uri;
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.photo_option_menu, menu);
+        return true;
     }
 }
